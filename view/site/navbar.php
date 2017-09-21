cc4f8b4decf131c53339432bdb61312b54f2320b
Losningen <schani@live.se>
2017-09-21 16:43:47 +0200

    Fixed one file missing DI

--- view/site/navbar.php
+++ view/site/navbar.php
@@ -28,7 +28,7 @@ $navbar = [
 <div class="container">
 <div class="header">
     <nav class="navbar navbar-dark bg-dark navbar-expand-md rounded">
-        <a class="navbar-brand" href="<?= $app->url->create($navbar["items"]["home"]["route"]) ?>">Lösningen</a>
+        <a class="navbar-brand" href="<?= $di->get("url")->create($navbar["items"]["home"]["route"]) ?>">Lösningen</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#containerNavbar" aria-controls="containerNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -37,7 +37,7 @@ $navbar = [
             <?php
             foreach ($navbar["items"] as $key) {
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link ' . ($app->request->getRoute() == $key["route"] ? "active" : "") . '" href="' . $app->url->create($key["route"]) . '">' . $key["title"] . '</a>';
+                echo '<a class="nav-link ' . ($di->get("request")->getRoute() == $key["route"] ? "active" : "") . '" href="' . $di->get("url")->create($key["route"]) . '">' . $key["title"] . '</a>';
                 echo '</li>';
             }
             ?>
